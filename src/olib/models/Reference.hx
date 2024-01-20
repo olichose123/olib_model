df779e6305093b36d018c639a1d0494bab6575b3
Olivier Banville <oli.banville@gmail.com>
2024-01-19 22:05:26 -0500

    create reference type

--- src/olib/models/Reference.hx
+++ src/olib/models/Reference.hx
@@ -9,32 +9,5 @@ typedef ModelStruct =
     public var type(default, null):String;
 }
 
-// @:genericBuild(olib.models.Macros.referenceMacro())
-class Reference<T:ModelStruct>
-{
-    public var name:String;
-    @:jignored public var instance:T;
-
-    public function new(instance:T, name:String = null)
-    {
-        this.instance = instance;
-        if (instance != null)
-        {
-            this.name = instance.name;
-        }
-        else
-        {
-            this.name = name;
-        }
-    }
-
-    public function toString():String
-    {
-        return name;
-    }
-
-    public function resolve():Void
-    {
-        // instance = cast Model.getInstance(this.type, name);
-    }
-}
+@:genericBuild(olib.models.Macros.referenceMacro())
+class Reference<T:ModelStruct> {}
