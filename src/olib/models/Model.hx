0e969678f4b8dd359bee6a8039ebc283b7fe41eb
Olivier Banville <olivier@banville.net>
2025-03-13 15:31:21 -0400

    add documentation

--- src/olib/models/Model.hx
+++ src/olib/models/Model.hx
@@ -2,17 +2,48 @@ package olib.models;
 
 import haxe.Exception;
 
+/**
+ * Base class for all models. Models are used to store data as JSON and provide serialization/deserialization functions.
+ * Models can be registered and retrieved by type and name.
+ * A model should extend Model and can implement any interface. A class should only inherit directly from Model,
+ * and not from another class that inherits from Model.
+**/
 @:autoBuild(olib.models.Macros.addNameAndTypeField())
 @:autoBuild(olib.models.Macros.addPublicFieldInitializers())
 @:autoBuild(olib.models.Macros.addJsonParser())
 @:autoBuild(olib.models.Macros.addJsonWriter())
 @:autoBuild(olib.models.Macros.addParserFunction())
 @:autoBuild(olib.models.Macros.addWriterFunction())
+@:autoBuild(olib.models.Macros.addGetFunction())
 class Model
 {
+    /**
+     * A map of all registered models. Access using the type of the model (usually MyModelClass.Type or myModelInstance.type).
+     * See also Model.get and Model.getAll.
+    **/
     public static final all:Map<String, Map<String, Model>> = new Map<String, Map<String, Model>>();
+
+    /**
+     * The duplicate handling behavior when registering a model with the same type and name as an existing model.
+     * Default is DuplicateHandling.Overwrite.
+     *
+     * - DuplicateHandling.Overwrite: Overwrite the existing model with the new model.
+     *   Call the onOverwrite function of the new model and the onOverwritten function of the old model.
+     * - DuplicateHandling.Ignore: Ignore the new model and keep the existing model.
+     * - DuplicateHandling.Error: Throw an error when a duplicate model is registered. Used when duplicates should not exist.
+     * - DuplicateHandling.Custom: Call the onCustomDuplicateHandling function of the existing model.
+     *   If it returns false, ignore the new model, otherwise overwrite the old model.
+    **/
     public static var duplicateHandling:DuplicateHandling = DuplicateHandling.Overwrite;
 
+    /**
+     * Register a new or existing model. This function is usually called automatically when deserializing a model or
+     * instantiating a new one.
+     * @param type
+     * @param instance
+     * @param altDuplicateHandling an alternative duplicateHandling behavior fetched from metadata when the model is auto-registered.
+     * @throws ModelException if a duplicate model is already registered and duplicateHandling is set to DuplicateHandling.Error.
+    **/
     public static function register(type:String, instance:Model, ?altDuplicateHandling):Void
     {
         var currDuplicateHandling:DuplicateHandling = duplicateHandling;
@@ -23,6 +54,11 @@ class Model
         switch (currDuplicateHandling)
         {
             case DuplicateHandling.Overwrite:
+                if (exists(type, instance.name))
+                {
+                    get(type, instance.name).onOverwritten(instance);
+                    instance.onOverwrite(get(type, instance.name));
+                }
 
             case DuplicateHandling.Ignore:
                 if (exists(type, instance.name))
@@ -35,6 +71,15 @@ class Model
                 {
                     throw new ModelException("Duplicate model: " + type + " " + instance.name);
                 }
+
+            case DuplicateHandling.Custom:
+                if (exists(type, instance.name))
+                {
+                    if (!instance.onCustomDuplicateHandling(get(type, instance.name)))
+                    {
+                        return;
+                    }
+                }
         }
         if (!all.exists(type))
         {
@@ -44,6 +89,11 @@ class Model
         instance.onRegister();
     }
 
+    /**
+     * Get all models of a certain type.
+     * @param type
+     * @return an iterator of all models of the specified type.
+    **/
     public static function getAll<T:Model>(type:String):Iterator<T>
     {
         if (!all.exists(type))
@@ -53,6 +103,12 @@ class Model
         return cast all.get(type).iterator();
     }
 
+    /**
+     * Get a model by type and name.
+     * @param type
+     * @param name
+     * @return the model if it exists, otherwise null.
+    **/
     public static function get<T:Model>(type:String, name:String):T
     {
         if (!all.exists(type))
@@ -62,6 +118,12 @@ class Model
         return cast all.get(type).get(name);
     }
 
+    /**
+     * Remove a model by type and name.
+     * @param type
+     * @param name
+     * @return true if the model was removed, false if it did not exist.
+    **/
     public static function remove(type:String, name:String):Bool
     {
         if (!all.exists(type))
@@ -71,6 +133,12 @@ class Model
         return all.get(type).remove(name);
     }
 
+    /**
+     * Check if a model exists by type and name.
+     * @param type
+     * @param name
+     * @return true if the model exists, false if it does not.
+    **/
     public static function exists(type:String, name:String):Bool
     {
         if (!all.exists(type))
@@ -80,6 +148,12 @@ class Model
         return all.get(type).exists(name);
     }
 
+    /**
+     * Deserializes a json string and checks the type field.
+     * Used to determine the type of a model. You can then use this to parse the json string into the correct model.
+     * @param json
+     * @return the type field of the json string, or null if it does not exist.
+    **/
     public static function peek(json:String):String
     {
         var obj:Dynamic = haxe.Json.parse(json);
@@ -95,11 +169,46 @@ class Model
         return type;
     }
 
+    /**
+     * The name of the model instance. Two models of the same type and name are considered duplicates.
+    **/
     public var name(default, null):String;
 
+    /**
+     * Callback when the model is registered, whether on instantiation or deserialization.
+    **/
     function onRegister():Void
     {
         //
+    }
+
+    /**
+     * Callback when a model is about to overwrite another model of the same type and name.
+     * @param oldModel the model about to be overwritten.
+    **/
+    function onOverwrite(oldModel:Model):Void
+    {
+        //
+    }
+
+    /**
+     * Callback when a model is about to be overwritten by another model of the same type and name.
+     * @param newModel
+    **/
+    function onOverwritten(newModel:Model):Void
+    {
+        //
+    }
+
+    /**
+     * Callback when a model is about to overwrite another model of the same type and name, but the duplicateHandling is set to DuplicateHandling.Custom.
+     * Return false to ignore the new model, true to overwrite the old model with the new model.
+     * @param oldModel the model about to be overwritten.
+     * @return true to overwrite the old model, false to ignore the new model.
+    **/
+    function onCustomDuplicateHandling(oldModel:Model):Bool
+    {
+        return true;
     }
 }
 
@@ -108,6 +217,7 @@ enum DuplicateHandling
     Overwrite;
     Ignore;
     Error;
+    Custom;
 }
 
-class ModelException extends Exception {}
+class extends Exception {}
--- src/olib/models/Reference.hx
+++ src/olib/models/Reference.hx
@@ -9,5 +9,10 @@ typedef ModelStruct =
     public var type(default, null):String;
 }
 
+/**
+ * A reference to a model. This is used to store a reference to a model as a name in string format in JSON.
+ * The default behavior of nested classes is to serialize the entire structure, which is not always desirable.
+ * Using a reference allows you to store a reference to a model by name while the type is stored in the class definition as Type Parameter T, and then retrieve the model by name later.
+**/
 @:genericBuild(olib.models.Macros.referenceMacro())
 class Reference<T:ModelStruct> {}
